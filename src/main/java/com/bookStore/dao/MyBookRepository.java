eb3a8330cfb6a45310deea142da01c09a905774d
Nabeel Siddiqui <nameisnabeel4@gmail.com>
2024-05-22 18:52:45 +0530

    first-commit

---
+++ src/main/java/com/bookStore/dao/MyBookRepository.java
@@ -0,0 +1,11 @@
+package com.bookStore.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bookStore.entity.MyBookList;
+
+@Repository
+public interface MyBookRepository extends JpaRepository<MyBookList,Integer>{
+
+}
